ff9329ebb6cbcb801f375d687496d66831817a18
Brian Jacobel <bjacobel@gmail.com>
2014-07-08 20:50:23 -0400

    Swapping out DRF for Tastypie (it's no longer actively maintained)

--- bongo/apps/bongo/api.py
+++
@@ -1,9 +0,0 @@
-# myapp/api.py
-from tastypie.resources import ModelResource
-from models import Post
-
-
-class PostResource(ModelResource):
-    class Meta:
-        queryset = Post.objects.all()
-        resource_name = 'post'
