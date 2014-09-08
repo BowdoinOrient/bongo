ce125c252a93b26ca006d4f10e0c4f757b037075
Brian Jacobel <bjacobel@gmail.com>
2014-09-07 22:23:55 -0400

    Don't need that hack now that 1.7 is out of beta

--- manage.py
+++ manage.py
@@ -7,10 +7,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bongo.settings.dev")
 
     from django.core.management import execute_from_command_line
-
-
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore",category=DeprecationWarning)
-        import django.contrib.contenttypes.generic
+    import django.contrib.contenttypes.generic
 
     execute_from_command_line(sys.argv)
