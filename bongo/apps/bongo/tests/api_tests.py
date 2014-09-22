f760a10526ff80eaa67744d5a519a4a0dfbd5f85
Brian Jacobel <bjacobel@gmail.com>
2014-09-21 23:02:25 -0400

    these are my api tests there are many like them but these are mine

--- bongo/apps/bongo/tests/api_tests.py
+++ bongo/apps/bongo/tests/api_tests.py
@@ -28,12 +28,17 @@ def crud(self, object, model, endpoint=None):
     # test that a GET to /endpoint/object.pk returns this object
     res = client.get(endpoint+str(object.pk)+"/")
     self.assertEqual(res.status_code, 200)
+    for key, value in json.loads(res.content).iteritems():
+        self.assertIn(key, dir(object))
+        if key == 'id':
+            self.assertEquals(object.pk, value)
 
     # test that a GET to /endpoint returns a list with this object on it
     # this doesn't actually work in production b/c of pagination, but there
     # aren't enough objects in the test DB to trigger pagination
-    res = client.get(endpoint)
+    res = client.get(endpoint+"?limit=100")
     self.assertEqual(res.status_code, 200)
+    self.assertIn(object.pk, [v for k, v in json.loads(res.content)[0].items() if k=='id'])
 
     # test that a POST to /endpoint with {object} 403s when not authenticated
     self.assertEqual(client.post(endpoint).status_code, 403)
@@ -51,25 +56,27 @@ def crud(self, object, model, endpoint=None):
     obj_as_dict = model_to_dict(object, exclude=['staticfile', 'profpic'])
 
     # test that a POST to /endpoint with {object} increases model.count by 1, when authenticated
+    count = type(object).objects.count()
     res = client.post(endpoint, obj_as_dict, secure=True)
     try:
         self.assertEqual(res.status_code, 201)
+        self.assertEqual(type(object).objects.count(), count+1)
     except AssertionError:
         if res.status_code == 400 and json.loads(res.content).get('staticfile') == [u'This field is required.']:
             # We don't really want to test file upload, so let this slide
             pass
 
-
     # test that a PUT to /endpoint/:newID makes a copy of object with id:newid
+    count = type(object).objects.count()
     tmp = obj_as_dict['id']
     obj_as_dict['id'] = 999
     res = client.put(endpoint+"999/", obj_as_dict, secure=True)
     try:
         self.assertEqual(res.status_code, 201)
+        self.assertEqual(type(object).objects.count(), count+1)
     except AssertionError:
         if res.status_code == 400 and json.loads(res.content).get('staticfile') == [u'This field is required.']:
             pass
-
     obj_as_dict['id'] = tmp
 
     # test that a PATCH to /endpoint/object.pk changes one of its fields to "derp", when authenticated
@@ -85,8 +92,10 @@ def crud(self, object, model, endpoint=None):
             pass
 
     # test that a DELETE to /endpoint/object.pk decreases model.count by 1, when authenticated
+    count = type(object).objects.count()
     res = client.delete(endpoint+str(object.pk)+"/", secure=True)
     self.assertEqual(res.status_code, 204)
+    self.assertEqual(type(object).objects.count(), count-1)
 
 
 class APITestCase(TestCase):
