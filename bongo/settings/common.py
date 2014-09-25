24188d3a0181fe0cb6e90156a8467e5beef2b7fd
Brian Jacobel <bjacobel@gmail.com>
2014-09-25 14:20:48 -0400

    Don't need CallingFormat with boto

--- bongo/settings/common.py
+++ bongo/settings/common.py
@@ -12,7 +12,6 @@ import os
 from datetime import timedelta
 from djcelery import setup_loader
 from os.path import join, normpath
-from S3 import CallingFormat
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -202,5 +201,4 @@ CORS_ORIGIN_WHITELIST = (
 AWS_ACCESS_KEY_ID = open(normpath(join(DJANGO_ROOT, 'settings/secrets/aws_id'))).read().strip()
 AWS_SECRET_ACCESS_KEY = open(normpath(join(DJANGO_ROOT, 'settings/secrets/aws_secret_key'))).read().strip()
 AWS_STORAGE_BUCKET_NAME = "bongo-static"
-AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
