915b3f3ca7008faa22bbe9ef32cd652d371dbe4a
Brian Jacobel <bjacobel@gmail.com>
2014-11-08 14:26:16 -0500

    Add a test to test the cache is working

---
+++ bongo/apps/bongo/tests/cache_tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.core.cache import cache
+
+class CacheTestCase(TestCase):
+    def test_cache_task(self):
+        """ Test memcached or LocMemCache, just so we know it's working """
+        cached = cache.get("cache_key")
+
+        if not cached:
+            cache.set("cache_key", "cache_value", 30)
+
+        self.assertEqual(cache.get("cache_key"), "cache_value")
