fa185caebfc5b1a88513dcab92ba5ce4fbe52c5d
Brian Jacobel <bjacobel@gmail.com>
2014-11-09 13:01:17 -0500

    No management commands in the frontend app any more

---
+++ bongo/apps/frontend/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from bongo.apps.bongo import models
+
+def HomeView(request):
+    pass
+
+def ArticleView(request):
+    pass
+
+def AuthorView(request):
+    pass
+
+def SeriesView(request):
+    pass
+
+def AboutView(request):
+    pass
+
+def EthicsView(request):
+    pass
+
+def SubscribeView(request):
+    pass
+
+def AdvertiseView(request):
+    pass
+
+def ContactView(request):
+    pass
+
+def TipView(request):
+    pass
