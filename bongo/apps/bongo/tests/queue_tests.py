5a396107d1f099a4625611660e1f91a25ebd6e7a
Brian Jacobel <bjacobel@gmail.com>
2015-04-01 22:29:05 -0400

    rq queueing test

---
+++ bongo/apps/bongo/tests/queue_tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from rq import Queue
+from redis import Redis
+from subprocess import Popen
+
+try:
+    from subprocess import DEVNULL as devnull
+except ImportError:
+    from os import devnull
+
+def add(x, y):
+    return x + y
+
+class QueueTestCase(TestCase):
+    def test_queued_code(self):
+        # make sure an rqworker is running
+        rqworker = Popen("rqworker", shell=True, stdout=devnull, stderr=devnull)
+
+        q = Queue(connection=Redis())
+        job = q.enqueue(add, 2, 2)
+
+        while not job.result:
+            pass
+
+        try:
+            self.assertEqual(add(2, 2), job.result)
+        finally:
+            # kill the rqworker, regardless of succcess
+            rqworker.terminate()
--- bongo/settings/common.py
+++ bongo/settings/common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 import os
 import sys
 import yaml
+from redis import Redis
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
