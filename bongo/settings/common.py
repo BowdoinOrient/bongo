dea5ff08de4b34c62d1bb427f1b186f4ed72a2c9
Brian Jacobel <bjacobel@gmail.com>
2015-04-15 09:21:40 -0400

    Django-compressor's HEAD is 1.8-compatible

--- bongo/settings/common.py
+++ bongo/settings/common.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 import os
 import sys
 import yaml
-from redis import Redis
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
