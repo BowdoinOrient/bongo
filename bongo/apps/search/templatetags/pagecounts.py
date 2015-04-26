f50fc29787bddd3e1bcd4ae6e4c17635efedef3a
Brian Jacobel <bjacobel@gmail.com>
2015-04-26 19:33:12 -0400

    Rip out Whoosh-specific template tag hacks

--- bongo/apps/search/templatetags/pagecounts.py
+++ bongo/apps/search/templatetags/pagecounts.py
@@ -5,7 +5,6 @@ register = template.Library()
 @register.filter(name='count')
 def count(page):
     return page.paginator._count
-    # return len([obj for obj in page.object_list if obj is not None])
 
 @register.filter(name='firstindex')
 def firstindex(page):
@@ -14,13 +13,3 @@ def firstindex(page):
 @register.filter(name='lastindex')
 def lastindex(page):
     return page.number * min(page.paginator.per_page, count(page))
-
-@register.filter(name='has_previous')
-def has_previous(page):
-    return page.has_previous
-
-@register.filter(name='has_next')
-def has_next(page):
-    if page.has_next and page.page_objects[page.paginator.per_page] is not None:
-        return True
-    return False
