9f16d3b321ecc569c00bc0993136a1897329895d
Brian Jacobel <bjacobel@localytics.com>
2015-09-12 16:31:15 -0400

    pep8 fixes in bongo/apps/api/pagination.py

--- bongo/apps/api/pagination.py
+++ bongo/apps/api/pagination.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 from rest_framework import pagination
 from rest_framework import serializers
 
+
 class LinksSerializer(serializers.Serializer):
     next = pagination.NextPageField(source='*')
     prev = pagination.PreviousPageField(source='*')
+
 
 class CustomPaginationSerializer(pagination.BasePaginationSerializer):
     links = LinksSerializer(source='*')  # Takes the page object as the source
